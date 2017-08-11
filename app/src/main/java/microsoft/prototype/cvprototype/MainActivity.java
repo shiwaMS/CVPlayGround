5023329d05a99646467d159d21321d70cdd9ea9a
Shiqi Wang <shiwa@microsoft.com>
2017-08-10 17:04:45 -0700

    first commit

---
+++ app/src/main/java/microsoft/prototype/cvprototype/MainActivity.java
@@ -0,0 +1,115 @@
+package microsoft.prototype.cvprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import microsoft.prototype.cvprototype.app.PermissionsManager;
+
+import static microsoft.prototype.cvprototype.app.PermissionsManager.Permission.CAMERA;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    JavaCameraView javaCameraView;
+    Mat mat, imageGray, imageCanny;
+
+    BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:
+                    if(javaCameraView != null){
+                        javaCameraView.enableView();
+                    }
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+        }else {
+            Log.d(TAG, "OpenCV loaded");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        this.javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        this.javaCameraView.setCvCameraViewListener(this);
+
+        PermissionsManager.INSTANCE.requestPermissionsIfNotGranted(this, CAMERA);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+            this.baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }else {
+            Log.d(TAG, "OpenCV loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_13, this, this.baseLoaderCallback);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(this.javaCameraView != null){
+            javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(this.javaCameraView != null){
+            this.javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        this.mat = new Mat(height, width, CvType.CV_8UC4);
+        this.imageGray = new Mat(height, width, CvType.CV_8UC1);
+        this.imageCanny = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        this.mat.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(Mat inputFrame) {
+        mat = inputFrame;
+
+        Imgproc.cvtColor(mat, imageGray, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.Canny(imageGray, imageCanny, 50, 150);
+        return imageCanny;
+    }
+}
---
+++ app/src/main/java/microsoft/prototype/cvprototype/app/PermissionsManager.java
@@ -0,0 +1,62 @@
+package microsoft.prototype.cvprototype.app;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum PermissionsManager {
+    INSTANCE;
+
+    public enum Permission {
+        CAMERA(0x01, Manifest.permission.CAMERA);
+
+        int requestCode;
+        String systemTag;
+
+        Permission(int requestCode, String systemTag) {
+            this.requestCode = requestCode;
+            this.systemTag = systemTag;
+        }
+    }
+
+    /**
+     * check if the required permissions are granted or not. If not, request the missing permissions
+     *
+     * @param context
+     * @param permissions
+     * @return true if all permissions have been granted, false otherwise
+     */
+    public boolean requestPermissionsIfNotGranted(Activity context, Permission... permissions) {
+        int requestCode = 0;
+        List<String> requestedPermissions = new ArrayList<>();
+
+        boolean granted = true;
+
+        for (Permission permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission.systemTag)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestCode |= permission.requestCode;
+                requestedPermissions.add(permission.systemTag);
+                granted = false;
+            }
+        }
+
+        if (!granted) {
+            ActivityCompat.requestPermissions(context,
+                    requestedPermissions.toArray(new String[requestedPermissions.size()]),
+                    requestCode);
+        }
+
+        return granted;
+    }
+}
