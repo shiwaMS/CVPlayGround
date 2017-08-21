27f29f37b005231cf1df761d72585d237a6dd07d
Shiqi Wang <shiwa@microsoft.com>
2017-08-21 09:32:26 -0700

    face only

--- app/src/main/jni/microsoft_prototype_cvprototype_app_NativeOpenCVClass.cpp
+++ app/src/main/jni/microsoft_prototype_cvprototype_app_NativeOpenCVClass.cpp
@@ -61,10 +61,10 @@ void detect(Mat &frame, const char *face_name, const char *eyes_name) {
         printf("--(!)Error loading\n");
         return;
     };
-    if (!eyes_cascade.load(eyes_cascade_name)) {
-        printf("--(!)Error loading\n");
-        return;
-    };
+//    if (!eyes_cascade.load(eyes_cascade_name)) {
+//        printf("--(!)Error loading\n");
+//        return;
+//    };
 
 
     std::vector<Rect> faces;
@@ -81,18 +81,18 @@ void detect(Mat &frame, const char *face_name, const char *eyes_name) {
         ellipse(frame, center, Size(faces[i].width * 0.5, faces[i].height * 0.5), 0, 0, 360,
                 Scalar(255, 0, 255), 4, 8, 0);
 
-        Mat faceROI = frame_gray(faces[i]);
-        std::vector<Rect> eyes;
+//        Mat faceROI = frame_gray(faces[i]);
+//        std::vector<Rect> eyes;
 
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
-
-        for (size_t j = 0; j < eyes.size(); j++) {
-            Point center(faces[i].x + eyes[j].x + eyes[j].width * 0.5,
-                         faces[i].y + eyes[j].y + eyes[j].height * 0.5);
-            int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
-            circle(frame, center, radius, Scalar(255, 0, 0), 4, 8, 0);
-        }
+//        //-- In each face, detect eyes
+//        eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+//
+//        for (size_t j = 0; j < eyes.size(); j++) {
+//            Point center(faces[i].x + eyes[j].x + eyes[j].width * 0.5,
+//                         faces[i].y + eyes[j].y + eyes[j].height * 0.5);
+//            int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
+//            circle(frame, center, radius, Scalar(255, 0, 0), 4, 8, 0);
+//        }
     }
 }
 
